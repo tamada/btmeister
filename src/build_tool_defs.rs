34cc3a86c75ffe4be047d72ed04c52ad95acf812
Haruaki Tamada <tamada@users.noreply.github.com>
2022-04-27 22:26:29 +0900

    feat: parse the given parameters

---
+++ src/build_tool_defs.rs
@@ -0,0 +1,70 @@
+use serde::{Deserialize, Serialize};
+use std::fs::OpenOptions;
+use std::io::BufReader;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct BuildToolDef {
+    name: String,
+    build_files: Vec<String>,
+    url: String,
+}
+
+pub type BuildToolDefs = std::vec::Vec<BuildToolDef>;
+
+impl BuildToolDef {
+    pub fn parse(path: PathBuf) -> Result<BuildToolDefs, Box<dyn std::error::Error>> {
+        let file = OpenOptions::new().read(true).open(path)?;
+        let reader = BufReader::new(file);
+        Ok(serde_json::from_reader(reader)?)
+    }
+
+    pub fn parse_from_pathstr(defs: String) -> Result<BuildToolDefs, Box<dyn std::error::Error>> {
+        BuildToolDef::parse(PathBuf::from_str(&defs)?)
+    }
+}
+
+pub fn mergeBuildTools(first: BuildToolDefs, second: BuildToolDefs) -> BuildToolDefs {
+    let mut result = BuildToolDefs::new();
+    for item in first {
+        result.push(item);
+    }
+    for item in second {
+        result.push(item);
+    }
+    result
+}
+
+pub fn construct(
+    defs: Option<PathBuf>,
+    append: Option<PathBuf>,
+) -> Result<BuildToolDefs, Box<dyn std::error::Error>> {
+    let def = if let Some(path) = defs {
+        BuildToolDef::parse(path)
+    } else {
+        BuildToolDef::parse_from_pathstr("defs/buildtools.json".to_string())
+    }?;
+    let result = if let Some(append_path) = append {
+        let additional_defs = BuildToolDef::parse(append_path)?;
+        mergeBuildTools(def, additional_defs)
+    } else {
+        def
+    };
+    Ok(result)
+}
+
+#[cfg(test)] 
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let r = BuildToolDef::parse_from_pathstr("defs/buildtools.json".to_string());
+        if let Ok(result) = r {
+            assert_eq!(23, result.len())
+        }
+    }
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::{ArgEnum, Parser};
-use thiserror::Error;
-use std::fs::File;
-use std::io::{BufReader, BufRead, stdin};
-use std::path::PathBuf;
 use std::error::Error;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+use std::path::{Path, PathBuf};
+use thiserror::Error;
+
+mod build_tool_defs;
 
 #[derive(Parser)]
 #[clap(
@@ -13,38 +15,56 @@ use std::error::Error;
     about = "Identifying the build tools of the projects in use."
 )]
 struct Options {
-    #[clap(long, value_name = "JSON", help = "Specify the additional definitions of the build tools.")]
+    #[clap(
+        long,
+        value_name = "JSON",
+        help = "Specify the additional definitions of the build tools."
+    )]
     append_defs: Option<PathBuf>,
 
-    #[clap(short, long, value_name = "JSON", help = "Specify the definition of the build tools.")]
+    #[clap(
+        short,
+        long,
+        value_name = "JSON",
+        help = "Specify the definition of the build tools."
+    )]
     definition: Option<PathBuf>,
 
-    #[clap(short, long, default_value = "default", value_name = "FORMAT", arg_enum, help = "Specify the output format")]
+    #[clap(
+        short,
+        long,
+        default_value = "default",
+        value_name = "FORMAT",
+        arg_enum,
+        help = "Specify the output format"
+    )]
     format: Format,
 
-    #[clap(short = '@', value_name = "INPUT", help = "Specify the file contains project path list. If INPUT is dash ('-'), read from STDIN.")]
+    #[clap(
+        short = '@',
+        value_name = "INPUT",
+        help = "Specify the file contains project path list. If INPUT is dash ('-'), read from STDIN."
+    )]
     project_list: Option<String>,
 
-    #[clap(value_name = "PROJECTs", required = false, help = "The target project directories for btmeister.")]
+    #[clap(
+        value_name = "PROJECTs",
+        required = false,
+        help = "The target project directories for btmeister."
+    )]
     dirs: Vec<PathBuf>,
 }
 
 impl Options {
     fn validate(&self) -> Option<MeisterError> {
-        if let Some(x) = &self.project_list {
-            if self.dirs.len() > 0 {
-                return Some(MeisterError::BothTargetSpecified())
-            } else {
-                None
-            }
+        if self.project_list.is_some() && !self.dirs.is_empty() {
+            Some(MeisterError::BothTargetSpecified())
+        } else if self.project_list.is_none() && self.dirs.is_empty() {
+            Some(MeisterError::NoProjectSpecified())
         } else {
-            if self.dirs.len() == 0 {
-                return Some(MeisterError::NoProjectSpecified())
-            } else {
-                None
-            }
+            None
         }
-    }    
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
@@ -56,13 +76,12 @@ enum Format {
 }
 
 struct BuildTool {
-    name: String,
-    build_files: Vec<String>,
-    url: String,
+    path: PathBuf,
+    def: build_tool_defs::BuildToolDef,
 }
 
 impl BuildTool {
-    fn parse(defs: String) -> Vec<BuildTool> {
+    fn parse(project: String) -> Vec<BuildTool> {
         Vec::new()
     }
 }
@@ -80,7 +99,7 @@ enum MeisterError {
 fn open_impl(file: String) -> Result<Box<dyn BufRead>, Box<dyn Error>> {
     match &*file {
         "-" => Ok(Box::new(BufReader::new(stdin()))),
-        _ => Ok(Box::new(BufReader::new(File::open(file)?)))
+        _ => Ok(Box::new(BufReader::new(File::open(file)?))),
     }
 }
 
@@ -90,11 +109,13 @@ fn parse_project_list(list_file: String) -> Result<Vec<PathBuf>, Box<dyn Error>>
     for line in f.lines() {
         lines.push(PathBuf::from(line.unwrap()));
     }
-    return Ok(lines)
+    Ok(lines)
 }
 
-
-fn parse_targets(project_list: Option<String>, dirs: Vec<PathBuf>) -> Result<Vec<PathBuf>, Box<dyn Error>> {
+fn parse_targets(
+    project_list: Option<String>,
+    dirs: Vec<PathBuf>,
+) -> Result<Vec<PathBuf>, Box<dyn Error>> {
     if let Some(x) = project_list {
         parse_project_list(x)
     } else {
@@ -102,12 +123,39 @@ fn parse_targets(project_list: Option<String>, dirs: Vec<PathBuf>) -> Result<Vec
     }
 }
 
+fn perform_each(
+    target: &Path,
+    _defs: &build_tool_defs::BuildToolDefs,
+) -> Result<i32, Box<dyn Error>> {
+    if !target.exists() {
+        Err(Box::new(MeisterError::ProjectNotFound(target.display().to_string())))
+    } else {
+
+        Ok(0)
+    }
+}
+
+fn perform(opts: Options) -> Result<i32, Box<dyn Error>> {
+    let defs = build_tool_defs::construct(opts.definition, opts.append_defs)?;
+    let targets = parse_targets(opts.project_list, opts.dirs)?;
+    for target in targets {
+        if let Err(e) = perform_each(target.as_path(), &defs) {
+            println!("{}", e);
+        }
+    }
+    Ok(0)
+}
 
 fn main() {
     let opts = Options::parse();
     if let Some(err) = opts.validate() {
         println!("{}", err)
     }
-    let _targets = parse_targets(opts.project_list, opts.dirs);
-
+    std::process::exit(match perform(opts) {
+        Err(err) => {
+            println!("{}", err);
+            1
+        }
+        Ok(code) => code,
+    })
 }
